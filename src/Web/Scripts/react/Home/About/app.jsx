07957becc8d68e409303dea927b48476877346bc
Ben Ward <benjamin.e.ward@outlook.com>
2020-04-20 00:53:19 -0500

    Add react footer styled component

--- src/Web/Scripts/react/Home/About/app.jsx
+++ src/Web/Scripts/react/Home/About/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Logos from "./Logos";
 import TechAccordion from "./TechAccordion";
+import Footer from "../../shared/Footer";
 import "../../../../Content/styles/app-style.scss";
 
 class App extends React.Component {
@@ -14,6 +15,14 @@ class App extends React.Component {
       <React.Fragment>
         <Logos />
         <TechAccordion />
+        <Footer>
+          <a
+            className="pull-right"
+            href="https://github.com/BenjaminEllisWard/demo-site"
+          >
+            See this site's source code here
+          </a>
+        </Footer>
       </React.Fragment>
     );
   }
