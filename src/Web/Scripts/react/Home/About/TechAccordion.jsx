4c74c52a3ebdfcd24cd0423b45991752d35f3048
Ben Ward <benjamin.e.ward@outlook.com>
2020-04-20 00:52:16 -0500

    Extract component from About/app.jsx

---
+++ src/Web/Scripts/react/Home/About/TechAccordion.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+import { BuildAboutTechInfoObject } from "../../../aboutHelperFunctions";
+import "../../../../Content/styles/app-style.scss";
+
+const techInfo = BuildAboutTechInfoObject();
+
+class TechAccordion extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selected: null,
+    };
+
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(index) {
+    this.setState({
+      selected: index === this.state.selected ? null : index,
+    });
+  }
+
+  render() {
+    return (
+      <Accordion>
+        {techInfo.map((x, index) => (
+          <div className="card-container" key={index}>
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                variant="link"
+                eventKey={index.toString()}
+                onClick={() => this.handleSelect(index)}
+              >
+                {x.title}
+                <div className="caret-container-wrapper pull-right">
+                  <div className="caret-container">
+                    <span
+                      className={`caret ${
+                        index === this.state.selected ? "open" : "closed"
+                      }`}
+                    />
+                  </div>
+                </div>
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={index.toString()}>
+              <React.Fragment>
+                {x.items.map((y, index) => (
+                  <div className="tech-item-container" key={index}>
+                    <strong>{y.title}: </strong>
+                    <span>{y.description}</span>
+                  </div>
+                ))}
+              </React.Fragment>
+            </Accordion.Collapse>
+          </div>
+        ))}
+      </Accordion>
+    );
+  }
+}
+
+export default TechAccordion;
