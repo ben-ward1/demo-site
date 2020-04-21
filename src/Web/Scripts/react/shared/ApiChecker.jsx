0cc819c2995f6146583f5a9c559c11bf69a0ca5c
Ben Ward <benjamin.e.ward@outlook.com>
2020-04-21 13:42:17 -0500

    Add error handling to react api checker

--- src/Web/Scripts/react/shared/ApiChecker.jsx
+++ src/Web/Scripts/react/shared/ApiChecker.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import axios from "axios";
-import { Spinner } from "react-bootstrap";
-import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
+import { Spinner, Popover, OverlayTrigger } from "react-bootstrap";
+import { faSyncAlt, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SuccessCheckIcon from "../shared/SuccessCheckIcon";
 import { BuildBaseUrl } from "../../urlHelperFunctions";
 
-library.add(faSyncAlt);
+library.add(faSyncAlt, faQuestionCircle);
+
+const popover = (
+  <Popover>
+    <Popover.Title as="h3">Oops!</Popover.Title>
+    <Popover.Content>
+      We didn't get a valid response from the api. This can happen for a number
+      of reasons, but I've probably just taken it's hosting instance down for
+      development purposes.
+    </Popover.Content>
+  </Popover>
+);
 
 class ApiChecker extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      someValue: null,
+      connected: null,
       errorMsg: null,
       loading: true,
     };
@@ -36,13 +47,13 @@ class ApiChecker extends React.Component {
   checkApiConnection() {
     const thisComponent = this;
 
-    this.setState({ someValue: null, errorMsg: null }, () => {
+    this.setState({ connected: null, errorMsg: null }, () => {
       setTimeout(function () {
         axios
           .get("home/test")
           .then((response) => {
             if (response.data) {
-              thisComponent.setState({ someValue: response.data });
+              thisComponent.setState({ connected: response.data.success });
             } else {
               throw "No response from api";
             }
@@ -52,6 +63,7 @@ class ApiChecker extends React.Component {
           .catch(function (e) {
             thisComponent.setState({
               errorMsg: "Oops, something went wrong: " + e,
+              loading: false,
             });
           });
         // set an arbitrary amount of time here just for demonstration
@@ -59,20 +71,37 @@ class ApiChecker extends React.Component {
     });
   }
 
+  BuildResponseText(connected, errorMsg) {
+    if (!errorMsg) {
+      return (
+        <div id="response-text" className={connected && "connected"}>
+          {connected ? "API: Connected!" : "API: Checking..."}
+        </div>
+      );
+    } else {
+      return (
+        <OverlayTrigger
+          trigger={["hover", "focus"]}
+          placement="bottom"
+          overlay={popover}
+        >
+          <div id="response-text">
+            <div>API: Down</div>
+            <FontAwesomeIcon icon={faQuestionCircle} />
+          </div>
+        </OverlayTrigger>
+      );
+    }
+  }
+
   render() {
-    const { errorMsg, someValue } = this.state;
+    const { errorMsg, connected } = this.state;
 
     return (
       <div id="api-response-container">
         <div id="api-response-item">
-          <SuccessCheckIcon id="api-check" check={someValue !== null} />
-          <div id="response-text" className={someValue && "connected"}>
-            {errorMsg
-              ? errorMsg
-              : someValue
-              ? "API: Connected!"
-              : "API: Checking..."}
-          </div>
+          <SuccessCheckIcon id="api-check" check={connected} />
+          {this.BuildResponseText(connected, errorMsg)}
           <div className="api-checker-action-container">
             {this.state.loading ? (
               <Spinner
