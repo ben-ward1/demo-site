c5966f9480d68b7738efd6af779631598acda5ba
Ben Ward <benjamin.e.ward@outlook.com>
2020-04-22 19:10:27 -0500

    Changes for release/publish

--- src/Web/App_Start/BundleConfig.cs
+++ src/Web/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace Web
             //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
             //          "~/Scripts/bootstrap/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/styles/css").Include(
                       //"~/Content/bootstrap/bootstrap.css",
                       "~/Content/bootstrap/site.css"));
         }
