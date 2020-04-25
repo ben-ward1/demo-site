d417d5ed33303b1cce2f9fd4fa685d27791b448d
Ben Ward <benjamin.e.ward@outlook.com>
2020-04-25 12:44:58 -0500

    Initial implementation of header notification

---
+++ src/Web/Scripts/notificationHelpers.js
@@ -0,0 +1,18 @@
+const firstVisitText =
+  "Thanks for visiting my website! This is a little something I put together to demonstrate " +
+  "the technologies I develop with. If you're a recruiter or hiring manager, I'm in the job market after being " +
+  "recently laid off due to COVID-19. I'm coming on two years of experience developing across the full stack, including " +
+  "UI work, REST API's, public facing websites, CRMs, third party integrations, and general web dev stuff. React + .NET/C# " +
+  "(Framework & Core) are my bread and butter, but you can take a look at my other skill areas on my resume (see Contact page). " +
+  "Take a look at the About page for an overview of the tech that I'm using for this site. Also see the contact page if you'd like " +
+  "to get in touch with me. If you just happened across this page for no reason, or are another developer who is curious about the " +
+  "development of this site, you can also feel free to reach out to me or to leave a message on the guestbook.";
+
+const firstVisitSubtext = "-Ben";
+
+const firstVisitMessageObject = {
+  mainText: firstVisitText,
+  subText: firstVisitSubtext,
+};
+
+export default firstVisitMessageObject;
--- src/Web/Scripts/react/Home/Index/app.jsx
+++ src/Web/Scripts/react/Home/Index/app.jsx
@@ -1,23 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HeaderNotification from "../../shared/headerNotification/HeaderNotification";
 import ApiChecker from "../../shared/ApiChecker";
 import GuestbookWizard from "../GuestbookWizard/GuestbookWizard";
 import "../../../../Content/styles/app-style.scss";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notificationIsOpen: true,
+    };
+
+    this.closeNotification = this.closeNotification.bind(this);
+  }
+
+  closeNotification() {
+    const el = document.getElementById("notification-container");
+    const cssString = `
+      height: 0px;
+      transition: height 0.5s
+    `;
+    el.style.cssText = cssString;
+
+    setTimeout(() => {
+      this.setState({ notificationIsOpen: false });
+    }, 250);
   }
 
   render() {
+    const { _isInitialArrivalForSession } = this.props;
+    const { notificationIsOpen } = this.state;
+
     return (
       <React.Fragment>
         <ApiChecker />
         <h2>Sign my guestbook</h2>
         <GuestbookWizard />
+        {/* {_isInitialArrivalForSession && <HeaderNotification />} */}
+        {notificationIsOpen && (
+          <HeaderNotification closeCallback={this.closeNotification} />
+        )}
       </React.Fragment>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App {...window.MODEL} />, document.getElementById("app"));
