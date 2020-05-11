6a9bdfac916516ea71718fa471824a67600b95bc
Ben Ward <benjamin.e.ward@outlook.com>
2020-05-10 20:03:26 -0500

    Remove unused bootstrap files

--- src/Web/App_Start/BundleConfig.cs
+++ src/Web/App_Start/BundleConfig.cs
@@ -8,22 +8,7 @@ namespace Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/bootstrap/jquery-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/bootstrap/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/bootstrap/modernizr-*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/styles/css").Include(
-                      //"~/Content/bootstrap/bootstrap.css",
                       "~/Content/bootstrap/site.css"));
         }
     }
